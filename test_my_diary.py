71948e220e5d01ce95a024cb60edee04652e47c6
C3real-kill3r <brybzi@gmail.com>
2018-07-18 15:18:11 +0300

    updated test and yml file

--- test_my_diary.py
+++ test_my_diary.py
@@ -22,25 +22,29 @@ class Test_Diary(unittest.TestCase):
     		response= cs.get('/api/v1/get_one/1',)
     		self.assertEqual(response.status_code, 200)
 
-    def test_logout(self):
-    	with app.test_client() as lo:
-    		response= lo.get('/api/v1/logout',)
-    		self.assertEqual(response.status_code, 200)
 
     def test_register(self):
         with app.test_client() as r:
             response=r.get('/api/v1/register',)
             self.assertEqual(response.status_code, 405)
-            self.assertEqual(r.post('/api/v1/login', json={"username":"brybz","password":"1234","email":"brybzi@gmail.com","name":"brian ryb"}).status_code, 200)
+            self.assertEqual(r.post('/api/v1/login', json={"name":"brian ryb","username":"brybz","email":"brybzi@gmail.com","password":"1234",}).status_code, 200)
 
     def test_login(self):
         with app.test_client() as l:
             response=l.get('/api/v1/login',)
             self.assertEqual(response.status_code, 405)
-            self.assertEqual(l.post('/api/v1/login', json={"username":"brybz","password":"1234"}).status_code, 500)
+            self.assertEqual(l.post('/api/v1/login', json={"username":"brybz","password":"1234"}).status_code, 200)
 
     def test_make_entry(self):
         with app.test_client() as m:
             response=m.get('/api/v1/login',)
             self.assertEqual(response.status_code, 405)    
             self.assertEqual(m.post('/api/v1/login', json={"entry":"lets try something out"}).status_code, 500)
+
+    def test_logout(self):
+        with app.test_client() as lo:
+            response= lo.get('/api/v1/logout',)
+            self.assertEqual(response.status_code, 200)
+
+if __name__=='__main__':
+    unittest.main()
