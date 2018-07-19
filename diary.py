0271dfef649c8bed25c06e0e2b4a98abf2245729
C3real-kill3r <brybzi@gmail.com>
2018-07-19 03:50:07 +0300

    modified file

--- diary.py
+++ diary.py
@@ -8,10 +8,12 @@ diary_content={}
 
 app.config['SECRET_KEY'] = 'brybzlee'
 
+#home page route
 @app.route ('/api/v1/',methods=['GET'])
 def home():
     return jsonify({'message' : 'welcome to your diary'})
 
+#registering a new user 
 @app.route ('/api/v1/register',methods=['POST'])
 def register():
     name =request.get_json()["name"]
@@ -19,7 +21,7 @@ def register():
     email =request.get_json()["email"]
     password =request.get_json()["password"]
 
-    if username not in user:
+    if username not in user:#checks if the username already exists
         user.update({username:{"name":name,"email":email,"password":password}})
         return jsonify(user)
     else:
@@ -31,6 +33,7 @@ def login_authorization(username, password):
             return True
     return False
 
+#a function to perfom deletion and insertion of an entry but still maintain the index position
 def replace (old,new,lst):
     for each in lst:
         if each == old:
@@ -38,7 +41,7 @@ def replace (old,new,lst):
             del lst[index]
             lst.insert(index,new)
 
-
+#check if a user is in session
 def logged_in(f):
     @wraps(f)
     def wrap(*args, **kwargs):
@@ -47,7 +50,7 @@ def logged_in(f):
         else:
             return f(*args, **kwargs)
     return wrap
-
+#logging into the diary
 @app.route ('/api/v1/login',methods=['POST'])
 def login():
     username=request.get_json()["username"]
@@ -59,6 +62,7 @@ def login():
     else:
         return jsonify({'message' : 'invalid credentials'})
 
+#making an entry
 @app.route ('/api/v1/make_entry',methods=['POST'])
 @logged_in
 def make_entry():
@@ -69,43 +73,49 @@ def make_entry():
     diary_content[username].append(entry)
     return jsonify({'message' : 'diary successfully updated'})
 
+#displaying all entries
 @app.route ('/api/v1/get_all',methods=['GET'])
 @logged_in
 def get_all():
     username=session.get('username')
     output={}
     for each in diary_content[username]:
-        output.update({diary_content[username].index(each)+1:each})
+        output.update({diary_content[username].index(each)+1:each})#displays all the entries by the user and updates a positional value by adding 1 to the index 
     return jsonify (output)
 
+#displaying one entry
 @app.route ('/api/v1/get_one/<int:entryID>',methods=['GET'])
 @logged_in
 def get_one(entryID):
-    username=session.get('username')
-    return jsonify({entryID:diary_content[username][entryID-1]})
+    username=session.get('username')#get's username from session
+    return jsonify({entryID:diary_content[username][entryID-1]})#displays the entry whose index in the list is less by 1 from the number entered as entryID
 
-
+#updating an already existing entry into the diary
 @app.route ('/api/v1/modify_entry/<int:entryID>',methods=['PUT'])
 @logged_in
 def modify_entry(entryID):
     entry=request.get_json()["entry"]
     username=session.get('username')
     old=diary_content[username][entryID-1]
-    replace (old, entry,diary_content[username])
+    replace (old, entry,diary_content[username])#invokes the replace function
     return jsonify({'message' :'entry successfully modified'})
 
+#deleting a single entry by a user
 @app.route ('/api/v1/delete_entry/<int:entryID>',methods=['DELETE'])
 @logged_in
 def delete_entry(entryID):
-    username=session.get('username')
-    del diary_content[username][entryID-1]
+    username=session.get('username')#get's username from the username stored in session
+    del diary_content[username][entryID-1]#deletes the entry whose index in the list is less by 1 from the number entered as entryID
     return jsonify ({'message' : 'succesfully deleted'})
 
+#ending a user session 
 @app.route ('/api/v1/logout',methods=['GET'])
 @logged_in
 def logout():
     session.clear()
     return jsonify({'message' : 'you are successfully logged out'})
 
+
+#script initialization
 if __name__== '__main__':
     app.run(debug=True)
