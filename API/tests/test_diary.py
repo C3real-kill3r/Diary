1d5a10c3e30e8402cab4cd0d4e57a39817997a41
C3real-kill3r <brybzi@gmail.com>
2018-07-25 10:51:24 +0300

    moved to a different directory

--- API/tests/test_diary.py
+++ API/tests/test_diary.py
@@ -12,12 +12,12 @@ class Test_Diary(unittest.TestCase):
             response = h.get('/ap1/v2/')
             self.assertEqual(response.status_code, 404)
 
-    def test_comment(self):
+    def test_view_all(self):
         with app.test_client() as c:
-            response= c.get('/api/v2/get_all',)
+            response= c.get('/api/v2/view_all',)
             self.assertEqual(response.status_code, 200)
 
-    def test_comment_single(self):
+    def test_comment_view_one(self):
         with app.test_client() as cs:
-            response= cs.get('/api/v2/get_one/1',)
+            response= cs.get('/api/v2/view_one/1',)
             self.assertEqual(response.status_code, 200)
