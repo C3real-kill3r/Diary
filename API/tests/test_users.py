095dc76416b6b775a4010a4b3a3a56aea6731359
C3real-kill3r <brybzi@gmail.com>
2018-08-03 01:48:20 +0300

    modified file stracture

--- API/tests/test_users.py
+++ API/tests/test_users.py
@@ -2,12 +2,27 @@ import os,sys
 
 sys.path.insert(0, os.path.abspath(".."))
 
-from flask import *
 import unittest
 import json
 from __init__ import *
 
 class Test_Users(unittest.TestCase):
+
+    def setUp(self):
+        connection = psycopg2.connect(dbname='diary', user=DB_USER, host='localhost', password=DB_PASS, port='5432')
+        with connection.cursor() as cursor:
+            cursor.execute ("CREATE TABLE IF NOT EXISTS entries (entryID serial PRIMARY KEY,\
+                username varchar(200) NOT NULL, title varchar(50) NOT NULL,\
+                comment text NOT NULL,comment_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP)")
+            cursor.execute ("CREATE TABLE IF NOT EXISTS users(id serial PRIMARY KEY,\
+                fname varchar(100), lname varchar(100),username varchar(100) NOT NULL,\
+                email varchar(100) NOT NULL,password varchar(100) NOT NULL,\
+                registration_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP)")
+            cursor.execute ("INSERT INTO users (fname, lname, username, email, password) VALUES ('brian', 'ryb', 'ken', 'brybzi@gmail.com',\
+             '76176fdf71023bf2af126f78ef3b2c4edbb01cd17524d39ed8c214be29ffa2c7')")
+        connection.commit()
+
+
 
     def test_wrong_method(self):
         self.assertEqual(app.test_client().post('/api/v2/').status_code, 405)
