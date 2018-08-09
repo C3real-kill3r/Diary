e581c6a25d1162b6a16bda0c368aeab6a6103bca
C3real-kill3r <brybzi@gmail.com>
2018-08-10 00:05:41 +0300

    modify fetch url

--- js/login.js
+++ js/login.js
@@ -1,20 +1,22 @@
-const loginPost = () => {
+document.getElementById("LoginForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+  const username = document.getElementById("username");
+  const password = document.getElementById("password");
 
-    fetch('https://diary234.herokuapp.com/api/v2/auth/login', {
-        method: 'POST',
-        body: JSON.stringify({
-          username: document.getElementById('username').value,
-          password: document.getElementById('password').value
-        }),
-        headers: {
-          'Content-type': 'application/json'
-        }
-      })
-      .then(response => response.json())
-      .then(json => {
-        console.log(json)
-      })
-      .catch(error => {
-          console.log(error);
-      });
-   }
+    credentials = {
+        username: username.value,
+        password: password.value
+    };
+    fetch("http://127.0.0.1:5000/api/v2/auth/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(credentials)
+        })
+        .then((response)=>{
+          response.json().then((data) => {
+              console.log(data)
+        })
+        .catch(err => console.log(err));
+  })});
--- js/register.js
+++ js/register.js
@@ -5,9 +5,8 @@ document.getElementById("Register").addEventListener("submit", function (event) 
   const username = document.getElementById("username");
   const email = document.getElementById("email");
   const password = document.getElementById("password");
-  const con_password = document.getElementById("confirm password");
+  const con_password = document.getElementById("con_password");
 
-  
     credentials = {
         fname: fname.value,
         lname: lname.value,
@@ -16,16 +15,21 @@ document.getElementById("Register").addEventListener("submit", function (event) 
         password: password.value,
         con_password: con_password.value
     };
-    fetch("https://diary234.herokuapp.com/api/v2/auth/signup", {
+    fetch("http://127.0.0.1:5000/api/v2/auth/signup", {
             method: "POST",
             headers: {
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
+                "Accept":"application/json"
             },
             body: JSON.stringify(credentials)
         })
         .then((response)=>{
           response.json().then((data) => {
-              console.log(data)  
+              console.log(data)
+              const RegResponse = Object(data.message)
+              let Message = document.getElementById("regResponse");
+              const FetchedMessage = `<p class"res">${RegResponse}</p>`
+              Message.innerHTML = FetchedMessage  
         })
         .catch(err => console.log(err));
-  });})
+  })});
